07df4677dded2bfd5974ec4a37e87b2091af6651
Krokogator <michal26262@gmail.com>
2016-01-22 08:00:19 +0100

    Dodane start menu v.1

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public Transform mainMenu, optionMenu;
+
+
+    public void LoadGame(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
