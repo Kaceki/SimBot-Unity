94fbf2e0b901198d667882f4b6fca1bcfe1f2810
Krokogator <michal26262@gmail.com>
2016-01-26 23:37:32 +0100

    Dodane rampy (again)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -274,10 +274,10 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        /*float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
+        rb.AddForce(movement * speed);*/
       
         if(i>0 && KnowWhatToDo)
         {
