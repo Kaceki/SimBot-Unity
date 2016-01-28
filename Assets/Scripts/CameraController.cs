51b8888030c4afef3f5f378d7c27b2c7337f772b
Krokogator <michal26262@gmail.com>
2016-01-28 22:29:31 +0100

    Przywrócona rotacja kamery :>

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     /*public GameObject player;
     private Vector3 offset;
-
 	// Use this for initialization
 	void Start () {
         offset = transform.position - player.transform.position;
@@ -14,32 +14,32 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate () {
         transform.position = player.transform.position + offset;
-
     }*/
-	public float turnSpeed = 4.0f;
-	public GameObject player;
-	
-	private Vector3 offset;
-	
+    public float turnSpeed = 4.0f;
+    public GameObject player;
+
+    private Vector3 offset;
+
     public float speedH = 4.0f;
     public float speedV = 2.0f;
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
 
-	void Start () {
-		offset = transform.position - player.transform.position;
-	}
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
 
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
-        yaw =  speedH* Input.GetAxis("Mouse X");
-        pitch = speedH*  Input.GetAxis("Mouse Y");
+        yaw = speedH * Input.GetAxis("Mouse X");
+        pitch = speedH * Input.GetAxis("Mouse Y");
         if (Input.GetKey(KeyCode.Mouse1))
         {
             //offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
-            offset = Quaternion.Euler(-pitch, yaw, 0)*offset;
+            offset = Quaternion.Euler(-pitch, yaw, 0) * offset;
 
             transform.position = player.transform.position + offset;
             transform.LookAt(player.transform.position);
@@ -48,9 +48,8 @@ public class CameraController : MonoBehaviour {
         {
             yaw += speedH * Input.GetAxis("Mouse X");
             pitch -= speedV * Input.GetAxis("Mouse Y");
-
             transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
         }*/
     }
-    }
+}
 
--- Assets/Scripts/CameraController2.cs
+++ Assets/Scripts/CameraController2.cs
@@ -11,13 +11,18 @@ public class CameraController2 : MonoBehaviour
     private Transform follow;
     private Vector3 targetPosition;
 
-    // Use this for initialization
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    public float speedH = 4.0f;
+    public float speedV = 2.0f;
+    private Vector3 offset;
+
     void Start()
     {
         follow = target.transform;
+        offset = transform.position - target.transform.position;   //cameracontroller1
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
@@ -27,7 +32,22 @@ public class CameraController2 : MonoBehaviour
 
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
 
-
+        //zwracanie kamery w kierunku BB8
         transform.LookAt(follow);
+
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            transform.position = target.transform.position + offset;
+            yaw = speedH * Input.GetAxis("Mouse X");
+            pitch = speedH * Input.GetAxis("Mouse Y");
+
+            //offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+            offset = Quaternion.Euler(-pitch, yaw, 0) * offset;
+
+            transform.position = target.transform.position + offset;
+            transform.LookAt(target.transform.position);
+
+        }
     }
 }
