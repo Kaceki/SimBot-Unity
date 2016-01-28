d40b5f3ba5e45dbf2e77ed8ddd4a9b7e6ed128ae
Krokogator <michal26262@gmail.com>
2016-01-28 21:48:37 +0100

    Nowy skrypt kamery, naprawiona podłoga w łazience, dodana rampa

---
+++ Assets/Scripts/CameraController2.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController2 : MonoBehaviour
+{
+    public GameObject target;
+
+    public float distanceAway;
+    public float distanceUp;
+    public float smooth;
+    private Transform follow;
+    private Vector3 targetPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        follow = target.transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+        /*Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
+        Debug.DrawLine(follow.position, targetPosition, Color.magenta);*/
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+
+
+        transform.LookAt(follow);
+    }
+}
